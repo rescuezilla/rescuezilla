142269a7fc878581aa49cb56365a0defb33e7dd1
Shasheen Ediriweera <shasheene@gmail.com>
2019-09-25 11:04:03 +0930

    Imports Redo Backup and Recovery snapshot-0.9.8.tar (released on 2011-03-10)
    Extracts Redo Backup and Recovery source code snapshot-0.9.8.tar [1] [2] using
    import.snapshot.tarballs.sh, into the src/ directory.  Given the tarball
    contains the complete source code for this release, to maintain correctness,
    the existing src/ directory was first deleted.
    
    The CHANGELOG entry for this release [3] is attached below, extracted from
    snapshot-1.0.1.tar [3], as it contained more information than the
    snapshot-0.9.8.tar version.  Please note, The release tarball does not contain
    any kind of build automation which would capture changes such as adding a new
    package with 'apt-get install'. An attempt has been made to reconstruct this
    information by examining the CHANGELOG, and the packages installed on the
    official ISO image uploaded to Sourceforge in 2011.
    
    The ISO image produced by building this commit successfully boots, reaches the
    desktop and launches the Redo Backup and Recovery frontend application. The
    contents of the ISO image build from this commit is nearly identical with the
    official ISO image, with all filesystem differences caused by the undocumented
    host system environment (eg /etc/hostname), timestamp dependent files (such as
    logs), and system scheduling based differences (cache files, compressed
    archives, python *.pyc compiled bytecode files). For completeness, the isolinux
    bootloader from the build environment is a slightly different version to the
    official ISO image (as checked with `strings isolinux.bin`). The bootloader is
    copied from the host operating system environment, which was not documented for
    the official ISO images. This change has no effect on functionality.
    
    -------------------------------------------------------------------------------
      Version 0.9.8 (2011-03-10)
    -------------------------------------------------------------------------------
      * Major platform shift; building from Ubuntu rather than xPUD in the future
      * Many base features not directly related to backup/restore have changed
      * Added boot menu option to check CD media for defects
      * Added performance enhancement section to /etc/smb.conf
      * Updated fsarchiver and partclone binaries to latest stable versions
      * Boot splash screen now displays version number for easy identification
    
    This commit was (initially) generated by import.snapshot.tarballs.sh.
    
    [1] sha256sum of snapshot-0.9.8.tar: ee8f0bac960b1e407848c462f2d526c5f3e7e709448f96893e6e96b9339f38fd
    [2] https://sourceforge.net/projects/redobackup/files/src/
        https://web.archive.org/web/20190921123610/https://sourceforge.net/projects/redobackup/files/src/
    [3] Extracted from snapshot-1.0.1.tar's file livecd/chroot/usr/share/redo/CHANGELOG

---
+++ src/livecd/chroot/etc/skel/.config/openbox/autostart.sh
@@ -0,0 +1,43 @@
+# This shell script is run before Openbox launches.
+# Environment variables set here are passed to the Openbox session.
+
+export OOO_FORCE_DESKTOP="gnome"
+export WINDOW_MANAGER="/usr/bin/openbox"
+export DESKTOP_SESSION="gnome"
+export GNOME_DESKTOP_SESSION_ID="openbox"
+export DE="openbox"
+
+# D-bus
+if which dbus-launch >/dev/null && test -z "$DBUS_SESSION_BUS_ADDRESS"; then
+       eval `dbus-launch --sh-syntax --exit-with-session`
+fi
+
+# set bg color
+xsetroot -solid "#333333"
+
+# turns NumLock on
+# numlockx on
+
+# eyes candy (composite)
+# myxcompmgr --startstop &
+
+# restore wallpaper
+nitrogen --restore &
+
+# network (gnome-network-manger)
+nm-applet &
+
+# power-manager
+#gnome-power-manager &
+
+# adeskbar (panel/systray)
+(sleep 1 && adeskbar)&
+
+# Mixer volume
+#(sleep 5 && volti)&
+
+# Start Redo Backup automatically
+redobackup &
+
+# Maximize all windows
+maximus &
