85503b41c3c0d31821b6f61fc35b412cc193c4f5
Shasheen Ediriweera <shasheene@gmail.com>
2021-01-01 16:08:31 +1030

    Allows checksumming of paths with spaces
    Ensures that the print_iso_checksums.sh helper script is able to checksum paths
    with spaces in it, which is the case when downloading multiple copies of the
    same file using a web browser.

--- print_iso_checksums.sh
+++ print_iso_checksums.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-SHA256=$(sha256sum $1)
-SHA1=$(sha1sum $1)
-MD5=$(md5sum $1)
+SHA256=$(sha256sum "$1")
+SHA1=$(sha1sum "$1")
+MD5=$(md5sum "$1")
 
 printf "sha256sum:\n$SHA256\n\n"
 printf "sha1sum:\n$SHA1\n\n"
