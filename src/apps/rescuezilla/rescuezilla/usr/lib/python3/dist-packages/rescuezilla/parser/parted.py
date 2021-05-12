9bad658ab0bd968487e3bf43bc26b62d48a3cc20
Shasheen Ediriweera <shasheene@gmail.com>
2021-05-12 13:30:47 +0930

    Updates copyright date to 2021
    Updates all relevant copyright dates to 2021.

--- src/apps/rescuezilla/rescuezilla/usr/lib/python3/dist-packages/rescuezilla/parser/parted.py
+++ src/apps/rescuezilla/rescuezilla/usr/lib/python3/dist-packages/rescuezilla/parser/parted.py
@@ -1,6 +1,6 @@
 # ----------------------------------------------------------------------
 #   Copyright (C) 2012 RedoBackup.org
-#   Copyright (C) 2019-2020 Rescuezilla.com <rescuezilla@gmail.com>
+#   Copyright (C) 2019-2021 Rescuezilla.com <rescuezilla@gmail.com>
 # ----------------------------------------------------------------------
 #   This program is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
