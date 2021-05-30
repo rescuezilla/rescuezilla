0e63e5ed64f9e7b3e2232f91d38ac051b8a58004
Shasheen Ediriweera <shasheene@gmail.com>
2021-05-30 19:48:27 +0930

    Fixes key check during post-MBR restore logic
    Ensures the post-MBR gap restore is done correctly for the relatively recent
    normalized ClonezillaImage format where MULTIDISK images are handled
    separately.

--- src/apps/rescuezilla/rescuezilla/usr/lib/python3/dist-packages/rescuezilla/restore_manager.py
+++ src/apps/rescuezilla/rescuezilla/usr/lib/python3/dist-packages/rescuezilla/restore_manager.py
@@ -509,14 +509,9 @@ class RestoreManager:
                 # calculating offsets to the first partition and comparing it to the post-MBR gap file size prevent
                 # accidentally overwriting it is NOT required here.
 
-                # There is a maximum of 1 post-MBR gap per drive (but there can be many drives)
-                post_mbr_gap_dict = None
-                for key in self.image.post_mbr_gap_dict.keys():
-                    if key.startswith(short_selected_image_drive_node):
-                        post_mbr_gap_dict = self.image.post_mbr_gap_dict[key]
-                if post_mbr_gap_dict is not None:
+                if 'absolute_path' in self.image.post_mbr_gap_dict.keys():
                     process, flat_command_string, failed_message = Utility.run("Restore post mbr gap",
-                                                                               ["dd", "if=" + post_mbr_gap_dict['absolute_path'],
+                                                                               ["dd", "if=" + self.image.post_mbr_gap_dict['absolute_path'],
                                                                                 "of=" + self.restore_destination_drive,
                                                                                 "seek=1",
                                                                                 "bs=512"],
