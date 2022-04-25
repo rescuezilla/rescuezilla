6fd0af5365f4823482d7aabedf14d684b834f447
regs01 <megaone@yandex.ru>
2022-04-26 01:43:51 +0300

    Update packages to jammy (22.04)

--- build.sh
+++ build.sh
@@ -310,7 +310,7 @@ fi
 # Create manifest
 chroot chroot dpkg-query -W --showformat='${Package} ${Version}\n' > image/casper/filesystem.manifest
 cp -v image/casper/filesystem.manifest image/casper/filesystem.manifest-desktop
-REMOVE='ubiquity ubiquity-frontend-gtk ubiquity-frontend-kde casper lupin-casper live-initramfs user-setup discover xresprobe os-prober libdebian-installer4'
+REMOVE='ubiquity ubiquity-frontend-gtk ubiquity-frontend-kde casper live-initramfs user-setup discover xresprobe os-prober libdebian-installer4'
 for i in $REMOVE; do
   sed -i "/${i}/d" image/casper/filesystem.manifest-desktop
 done
