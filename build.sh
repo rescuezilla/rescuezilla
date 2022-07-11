d9341792ebfbf4a3af095ad9a29c64db997ce124
Shasheen Ediriweera <shasheene@gmail.com>
2022-07-10 18:28:03 -0700

    Switch loop over bash string to loop over array
    Switches looping over a bash space-separated string to looping over an array,
    as this means more reviewable diff's in subsequent commits.

--- build.sh
+++ build.sh
@@ -310,9 +310,21 @@ fi
 # Create manifest
 chroot chroot dpkg-query -W --showformat='${Package} ${Version}\n' > image/casper/filesystem.manifest
 cp -v image/casper/filesystem.manifest image/casper/filesystem.manifest-desktop
-REMOVE='ubiquity ubiquity-frontend-gtk ubiquity-frontend-kde casper lupin-casper live-initramfs user-setup discover xresprobe os-prober libdebian-installer4'
-for i in $REMOVE; do
-  sed -i "/${i}/d" image/casper/filesystem.manifest-desktop
+REMOVE=("ubiquity"
+        "ubiquity-frontend-gtk"
+        "ubiquity-frontend-kde"
+        "casper"
+        "lupin-casper"
+        "live-initramfs"
+        "user-setup"
+        "discover"
+        "xresprobe"
+        "os-prober"
+        "libdebian-installer4"
+)
+for remove in "${REMOVE[@]}"
+do
+     sed -i "/${remove}/d" image/casper/filesystem.manifest-desktop
 done
 
 cat << EOF > image/README.diskdefines
