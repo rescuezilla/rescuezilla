a2695ed266baef052e453b729f6d37d8712db9f1
Shasheen Ediriweera <shasheene@gmail.com>
2022-07-17 19:00:21 -0700

    Moves the git add safe directory command to script

---
+++ git_add_safe_directory.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Echo each command
+set -x
+
+# Directory containing this build script
+BASEDIR=$(dirname $(readlink -f "$0"))
+
+git config --global --add safe.directory ${BASEDIR}/
+git config --global --add safe.directory ${BASEDIR}/src/third-party/partclone
+
