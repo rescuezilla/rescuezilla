4374ccf63a8f8ae5dbb956700f4ff92d8537c853
Shasheen Ediriweera <shasheene@gmail.com>
2022-08-13 07:53:14 -0700

    Remove trailing "/" git add safe dir script
    Fixes the add safe directory script by removing trailing forward slash.

--- git_add_safe_directory.sh
+++ git_add_safe_directory.sh
@@ -6,6 +6,6 @@ set -x
 # Directory containing this build script
 BASEDIR=$(dirname $(readlink -f "$0"))
 
-git config --global --add safe.directory ${BASEDIR}/
+git config --global --add safe.directory ${BASEDIR}
 git config --global --add safe.directory ${BASEDIR}/src/third-party/partclone-latest
-git config --global --add safe.directory ${BASEDIR}/src/third-party/partclone.v0.2.43/
+git config --global --add safe.directory ${BASEDIR}/src/third-party/partclone.v0.2.43
