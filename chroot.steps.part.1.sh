rescuezilla/rescuezilla

commit a521780135f98d6c4ee9181514dfc95e647ccde4

Adds build based on Ubuntu 22.04 (Jammy)
"ttf-ubuntu-font-family" is deprecated. fonts-ubuntu is the right one.

"exfat-utils" is replaced with exfatprogs

chroot.steps.part.1.sh
@@ -105,6 +105,22 @@ pkgs_specific_to_ubuntu2110_impish=(
                        "lupin-casper"
 )
 
+pkgs_specific_to_ubuntu2204_jammy=(
+			"linux-generic"
+                        "xserver-xorg"
+                        "xserver-xorg-video-all"
+                        "xserver-xorg-video-intel"
+                        "xserver-xorg-video-qxl"
+                        "xserver-xorg-video-mga"
+                        # Packages which may assist users needing to do a GRUB repair (64-bit EFI)
+                       "shim-signed"
+                       "grub-efi-amd64-signed"
+                       "grub-efi-amd64-bin"
+                       "grub-efi-ia32-bin"
+                       # Dependency for partclone-utils' imagemount
+                       "nbdkit"
+)
+
 # Languages on the system
 lang_codes=(
              "ar"
@@ -185,7 +201,7 @@ common_pkgs=("discover"
              "arandr"
              "xfce4-terminal"
              "lxpanel"
-             "ttf-ubuntu-font-family"
+             "fonts-ubuntu"
              "alsamixergui"
              "volumeicon-alsa"
              "pm-utils"
@@ -225,7 +241,7 @@ common_pkgs=("discover"
              "jfsutils"
              "wget"
              "exfat-fuse"
-             "exfat-utils"
+             "exfatprogs"
              "btrfs-progs"
              "udisks2-btrfs"
              "hfsplus"
@@ -253,6 +269,8 @@ elif  [ "$ARCH" == "amd64" ] && [ "$CODENAME" == "focal" ]; then
   apt_pkg_list=("${pkgs_specific_to_ubuntu2004_focal[@]}" "${common_pkgs[@]}")
 elif  [ "$ARCH" == "amd64" ] && [ "$CODENAME" == "impish" ]; then
   apt_pkg_list=("${pkgs_specific_to_ubuntu2110_impish[@]}" "${common_pkgs[@]}")
+elif  [ "$ARCH" == "amd64" ] && [ "$CODENAME" == "jammy" ]; then
+  apt_pkg_list=("${pkgs_specific_to_ubuntu2204_jammy[@]}" "${common_pkgs[@]}")
 else
   echo "Warning: unknown CPU arch $ARCH or Ubuntu release codename $CODENAME"
   exit 1

Makefile
@@ -1,5 +1,5 @@
-.DEFAULT_GOAL := focal
-.PHONY: all focal impish i386 deb sfdisk.v2.20.1.amd64 partclone.restore.v0.2.43.amd64 partclone-utils partclone-nbd install test integration-test clean-build-dir clean clean-all
+.DEFAULT_GOAL := jammy
+.PHONY: all focal impish jammy i386 deb sfdisk.v2.20.1.amd64 partclone.restore.v0.2.43.amd64 partclone-utils partclone-nbd install test integration-test clean-build-dir clean clean-all
 
 # FIXME: Properly specify the build artifacts to allow the GNU make to actually be smart about what gets built and when.
 # FIXME: This lack of specifying dependency graph means requires eg, `make focal` and `make impish` has to be done as separate invocations
@@ -32,6 +32,12 @@ impish: CODENAME=impish
 export ARCH CODENAME
 impish: deb sfdisk.v2.20.1.amd64 partclone.restore.v0.2.43.amd64 partclone-utils partclone-nbd $(buildscripts)
 	BASE_BUILD_DIRECTORY=$(BASE_BUILD_DIRECTORY) ./build.sh
+	
+jammy: ARCH=amd64
+jammy: CODENAME=jammy
+export ARCH CODENAME
+jammy: deb sfdisk.v2.20.1.amd64 partclone-utils partclone-nbd $(buildscripts)
+	BASE_BUILD_DIRECTORY=$(BASE_BUILD_DIRECTORY) ./build.sh	
 
 # ISO image based on Ubuntu 18.04 Bionic LTS (Long Term Support) 32bit (the last 32bit/i386 Ubuntu LTS release)
 i386: ARCH=i386

commit 94deac5b499af0971d13694029397ad080875467

Install exfat-utils to fix Focal, Impish builds
Fix Focal, Impish builds broken in the #339 upgrade to Jammy, by installing
exfat-utils on those images, and exfatprogs on Jammy.

chroot.steps.part.1.sh
@@ -86,6 +86,8 @@ pkgs_specific_to_ubuntu2004_focal=("linux-generic-hwe-18.04"
                        # Dependency for partclone-utils' imagemount
                        "nbdkit"
                        "lupin-casper"
+                       # Replaced by exfatprogs
+                       "exfat-utils"
 )
 
 pkgs_specific_to_ubuntu2110_impish=(
@@ -103,6 +105,8 @@ pkgs_specific_to_ubuntu2110_impish=(
                        # Dependency for partclone-utils' imagemount
                        "nbdkit"
                        "lupin-casper"
+                       # Replaced by exfatprogs
+                       "exfat-utils"
 )
 
 pkgs_specific_to_ubuntu2204_jammy=(
@@ -119,6 +123,8 @@ pkgs_specific_to_ubuntu2204_jammy=(
                        "grub-efi-ia32-bin"
                        # Dependency for partclone-utils' imagemount
                        "nbdkit"
+                       # Replaces exfat-utils
+                       "exfatprogs"
 )
 
 # Languages on the system
@@ -246,7 +252,6 @@ common_pkgs=("discover"
              "jfsutils"
              "wget"
              "exfat-fuse"
-             "exfatprogs"
              "btrfs-progs"
              "udisks2-btrfs"
              "hfsplus"