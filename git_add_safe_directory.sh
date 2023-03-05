b8acf41aacb074de1904038731732cc7fff04031
Shasheen Ediriweera <shasheene@gmail.com>
2023-03-05 13:18:59 -0800

    Set util-linux module path with base dir variable

--- git_add_safe_directory.sh
+++ git_add_safe_directory.sh
@@ -9,4 +9,4 @@ BASEDIR=$(dirname $(readlink -f "$0"))
 git config --global --add safe.directory ${BASEDIR}
 git config --global --add safe.directory ${BASEDIR}/src/third-party/partclone-latest
 git config --global --add safe.directory ${BASEDIR}/src/third-party/partclone.v0.2.43
-git config --global --add safe.directory /home/rescuezilla/src/third-party/util-linux
+git config --global --add safe.directory ${BASEDIR}/src/third-party/util-linux
