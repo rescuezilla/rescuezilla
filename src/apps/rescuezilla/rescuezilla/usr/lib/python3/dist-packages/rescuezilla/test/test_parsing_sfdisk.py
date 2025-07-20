fc83af159cdd8a1d6d854bc17676b4faf0c61e42
Shasheen Ediriweera <1414379+shasheene@users.noreply.github.com>
2025-07-20 12:31:33 -0700

    Fix and re-enable 'sfdisk' parser test
    Fixes the assertion of the sfdisk unit test:
    
    * Use underscores on dictionary key over dashes
    
    * Add missing 'unit' fields
    
    * Use an ordered dictionary for the partitions field, not a regular dictionary

--- src/apps/rescuezilla/rescuezilla/usr/lib/python3/dist-packages/rescuezilla/test/test_parsing_sfdisk.py
+++ src/apps/rescuezilla/rescuezilla/usr/lib/python3/dist-packages/rescuezilla/test/test_parsing_sfdisk.py
@@ -16,6 +16,7 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 # ----------------------------------------------------------------------
+import collections
 import pprint
 import unittest
 
@@ -42,30 +43,42 @@ last-lba: 16223214
         sfdisk_dict = Sfdisk.parse_sfdisk_dump_output(input_sfdisk_string)
         expected_sfdisk_dict = {
             "label": "gpt",
-            "label-id": "20B6B2AC-F20E-48A7-83C5-6B684328AFAB",
+            "label_id": "20B6B2AC-F20E-48A7-83C5-6B684328AFAB",
             "device": "/dev/sdb",
-            "first-lba": 34,
-            "last-lba": 16223214,
-            "partitions": {
-                "/dev/sdb1": {
-                    "start": 2048,
-                    "size": 1849344,
-                    "type": "0FC63DAF-8483-4772-8E79-3D69D8477DE4",
-                    "uuid": "ABEC187C-BD81-4F2F-B8C0-0DB4892055E2",
-                },
-                "/dev/sdb2": {
-                    "start": 1851392,
-                    "size": 1351680,
-                    "type": "EBD0A0A2-B9E5-4433-87C0-68B6B72699C7",
-                    "uuid": "9A0ECA81-6D1A-497E-8614-B70404C37918",
-                },
-                "/dev/sdb3": {
-                    "start": 3203072,
-                    "size": 13019136,
-                    "type": "EBD0A0A2-B9E5-4433-87C0-68B6B72699C7",
-                    "uuid": "B65EDC17-2C20-46B0-BD65-C92167FFEEC2",
-                },
-            },
+            "unit": "sectors",
+            "first_lba": 34,
+            "last_lba": 16223214,
+            "partitions": collections.OrderedDict(
+                [
+                    (
+                        "/dev/sdb1",
+                        {
+                            "start": 2048,
+                            "size": 1849344,
+                            "type": "0FC63DAF-8483-4772-8E79-3D69D8477DE4",
+                            "uuid": "ABEC187C-BD81-4F2F-B8C0-0DB4892055E2",
+                        },
+                    ),
+                    (
+                        "/dev/sdb2",
+                        {
+                            "start": 1851392,
+                            "size": 1351680,
+                            "type": "EBD0A0A2-B9E5-4433-87C0-68B6B72699C7",
+                            "uuid": "9A0ECA81-6D1A-497E-8614-B70404C37918",
+                        },
+                    ),
+                    (
+                        "/dev/sdb3",
+                        {
+                            "start": 3203072,
+                            "size": 13019136,
+                            "type": "EBD0A0A2-B9E5-4433-87C0-68B6B72699C7",
+                            "uuid": "B65EDC17-2C20-46B0-BD65-C92167FFEEC2",
+                        },
+                    ),
+                ]
+            ),
         }
         print("actual:" + str(sfdisk_dict))
         self.assertDictEqual(expected_sfdisk_dict, sfdisk_dict)
